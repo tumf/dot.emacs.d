35a3514e0fbf721492914b6db322a5960d899d2b
Yoshihiro Takahara <y.takahara@gmail.com>
2013-07-06 15:21:53 +0900

    add auto-save-buffers-enhanced

---
+++ utilities.el
@@ -0,0 +1,9 @@
+;; (Utilities) ;;
+
+;; AutoSaveBuffersEnhanced
+(push "~/.emacs.d/utilities/auto-save-buffers-enhanced" load-path)
+(require 'auto-save-buffers-enhanced)
+(auto-save-buffers-enhanced t)
+
+
+
