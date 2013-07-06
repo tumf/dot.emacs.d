2828f5347e592e998c46594f810ee5124a172eb3
Yoshihiro Takahara <y.takahara@gmail.com>
2013-07-06 15:34:28 +0900

    settings for ruby-mode

--- modes.el
+++ modes.el
@@ -1,5 +1,12 @@
 ;; (Modes) ;;
 
+;; Ruby
+(autoload 'ruby-mode "ruby-mode"  "Mode for editing ruby source files" t)
+(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Berksfile$" . ruby-mode))
+
 ;; rspec
 (add-hook 'rspec-mode-hook
           '(lambda ()
