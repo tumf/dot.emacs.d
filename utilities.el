582fa370e7a5fbfd6f3d0530d6441148d04b6ea9
Yoshihiro Takahara <y.takahara@gmail.com>
2013-07-07 02:39:38 +0900

    .

--- utilities.el
+++ utilities.el
@@ -1,27 +1,33 @@
 ;; (Utilities) ;;
+(push "~/.emacs.d/utilities" load-path)
 
 ;; AutoSaveBuffersEnhanced
 (push "~/.emacs.d/utilities/auto-save-buffers-enhanced" load-path)
 (require 'auto-save-buffers-enhanced)
 (auto-save-buffers-enhanced t)
 
+
+;; yasnippets
+(require 'yasnippet)
+(setq yas-snippet-dirs
+      '(
+        "~/.emacs.d/el-get/rspec-mode/snippets"
+        "~/.emacs.d/el-get/yasnippet/snippets"
+        ))
+(yas-global-mode 1)
+
 ;; anything
 (require 'anything-config)
+;;(require 'anything-c-yasnippet)
 (global-set-key [?\C-;] 'anything)
 (setq anything-sources
      (list
       anything-c-source-imenu
+      ;;anything-c-source-yasnippet
       anything-c-source-files-in-current-dir
       anything-c-source-buffers
       anything-c-source-recentf
       anything-c-source-file-name-history
       anything-c-source-emacs-commands
       ))
-
-
-
-
-
-
-
 
