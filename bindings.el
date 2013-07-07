c1f213a56d543a43aebd1d1ecb7ddba6826c5365
Yoshihiro Takahara <y.takahara@gmail.com>
2013-07-07 13:57:51 +0900

    Keybinding for Mac

--- bindings.el
+++ bindings.el
@@ -1,8 +1,13 @@
 ;; (Key Bindings) ;;
-(setq mac-command-modifier 'meta)
+
 (setq-default tab-width 2 indent-tabs-mode nil)
 (global-set-key "\C-h" 'delete-backward-char)
 (global-set-key "\C-cg" 'magit-status)
 (global-set-key "\M-g" 'goto-line)
 (global-set-key "\C-w" 'kill-region)
 (tool-bar-mode -1)
+
+;; for Mac
+(if (eq system-type 'darwin)
+    (setq mac-command-modifier 'meta))
+
