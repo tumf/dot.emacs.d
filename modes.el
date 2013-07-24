4e1670b77cfd519ab75ad677f2759f4a54019524
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2013-07-24 20:15:44 +0900

    phpのインデント設定

--- modes.el
+++ modes.el
@@ -23,3 +23,11 @@
 ;;             (local-set-key "\C-c\C-c" 'rspec-verify-single)))
 ;;(eval-after-load 'rspec-mode '(rspec-install-snippets))
 ;;(yas/load-directory "~/.emacs.d/el-get/rspec-mode/snippets")
+(defun php-indent-hook()
+  (c-set-style "stroustrup")
+  (setq indent-tabs-mode nil)
+  (setq c-basic-offset 2)
+  ;;(c-set-offset 'case-label '+) ; switch文のcaseラベル
+  (c-set-offset 'arglist-intro '+) ; 配列の最初の要素が改行した場合
+  (c-set-offset 'arglist-close 0)) ; 配列の閉じ括弧
+(add-hook 'php-mode-hook 'php-indent-hook)
