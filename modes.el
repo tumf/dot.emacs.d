77ff62b4f29071be94443ec49141f4db10772a3a
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2013-12-09 01:12:49 +0900

    *.rakeをruby-mode

--- modes.el
+++ modes.el
@@ -3,6 +3,7 @@
 ;; Ruby
 (autoload 'ruby-mode "ruby-mode"  "Mode for editing ruby source files" t)
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
