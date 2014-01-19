1914044e891e90851fb52c3cd76ce37ba331a39a
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2014-01-19 15:06:12 +0900

    font-sizeを小さく

--- themes.el
+++ themes.el
@@ -6,10 +6,10 @@
 ;; Font
 ;; from http://sakito.jp/emacs/emacs23.html
 (when (display-graphic-p)
-  (create-fontset-from-ascii-font "Menlo-14:weight=normal:slant=normal" nil "menlokakugo")
+  (create-fontset-from-ascii-font "Menlo-12:weight=normal:slant=normal" nil "menlokakugo")
   (set-fontset-font "fontset-menlokakugo"
                     'unicode
-                    (font-spec :family "Hiragino Kaku Gothic ProN" :size 16)
+                    (font-spec :family "Hiragino Kaku Gothic ProN" :size 14)
                     nil
                     'append)
   (add-to-list 'default-frame-alist '(font . "fontset-menlokakugo"))
