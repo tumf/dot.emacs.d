a4ea90c35273914f1f5e7eb51427c3a4b771bbdc
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2014-01-19 15:04:57 +0900

    themeが正しく読み込まれるよう最後に持ってきた

--- init.el
+++ init.el
@@ -61,10 +61,10 @@
 (push "~/.emacs.d/bin" exec-path)
 
 (load "emacs.el")
-(load "themes.el")
 (load "modes.el")
 (load "utilities.el")
 (load "bindings.el")
+(load "themes.el")
 
 ;; If ~/.emacs.local is available load it as the last file so that it is
 ;; possible to add local settings and overrides.
