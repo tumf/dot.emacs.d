d8098aa51a69bf619de68c62cb1b0bc6f133bb5a
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2014-01-19 14:20:56 +0900

    resolved #2

--- themes.el
+++ themes.el
@@ -5,13 +5,15 @@
 
 ;; Font
 ;; from http://sakito.jp/emacs/emacs23.html
-(create-fontset-from-ascii-font "Menlo-14:weight=normal:slant=normal" nil "menlokakugo")
-(set-fontset-font "fontset-menlokakugo"
-                  'unicode
-                  (font-spec :family "Hiragino Kaku Gothic ProN" :size 16)
-                  nil
-                  'append)
-(add-to-list 'default-frame-alist '(font . "fontset-menlokakugo"))
+(when (display-graphic-p)
+  (create-fontset-from-ascii-font "Menlo-14:weight=normal:slant=normal" nil "menlokakugo")
+  (set-fontset-font "fontset-menlokakugo"
+                    'unicode
+                    (font-spec :family "Hiragino Kaku Gothic ProN" :size 16)
+                    nil
+                    'append)
+  (add-to-list 'default-frame-alist '(font . "fontset-menlokakugo"))
+)
 
 
 
