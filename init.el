7bf4798148d156bce7aab2a426b50869cc61cbdb
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2015-08-21 15:30:30 +0900

    mepa based

--- init.el
+++ init.el
@@ -1,109 +1,188 @@
+;; my packages to be install
+(defvar my-packages '(
+                      better-defaults
+                      paredit
+                      idle-highlight-mode
+                      ido-ubiquitous
+                      find-file-in-project
+                      magit
+                      smex
+                      scpaste
+                      wakatime-mode
+                      helm helm-descbinds helm-ag
+                      markdown-mode
+                      smart-newline
+                      flycheck
+                      ))
+
 (require 'package) ;; You might already have this line
 
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+
 (package-initialize)
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
 
-;;(push "/usr/local/bin" exec-path)
-;;https://github.com/dholm/dotemacs/blob/master/.emacs.d/init.el
-(setq initial-frame-alist
-      '((top . 1) (left . 1) (width . 160) (height . 60)))
-(cd "~/")
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+;; 外部コマンドのPATH
+;; この設定が利用する主なコマンド `git` `ag`
+(push "/usr/local/bin" exec-path)
 
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (let (el-get-master-branch)
-      (goto-char (point-max))
-      (eval-print-last-sexp))))
-
-(setq el-get-user-package-directory "~/.emacs.d/init")
-(setq el-get-sources
-      '(;; Code helpers
-        ;;auto-complete cedet dtrt-indent ecb flymake-cursor
-        ;;google-c-style smart-tab ensime scion pymacs jedi perl-completion
-        google-c-style
-        auto-complete
-        auto-complete-ruby
-        yasnippet auto-complete-yasnippet
-        neotree
-        ;; Modes
-        ruby-mode rvm haml-mode rhtml-mode
-        ruby-electric
-        smart-newline
-        php-mode
-        markdown-mode
-        yaml-mode
-        multiple-cursors smartrep
-        sass-mode
-        ;;gnuplot-mode haskell-mode js2-mode markdown-mode nxhtml rainbow-mode
-        ;;wc-mode python-mode pylookup slime php-mode scala-mode2 cperl-mode
-        ;;jinja rvm
-
-        ;; Version Control Systems
-        magit
-        ;;vc-clearcase git-gutter-fringe
-
-        ;; Utilities
-        ;;deft lusty-explorer profile-dotemacs sunrise-commander elim perspective
-        shell-command
-        ack
-        ;;emacs-w3m
-
-        ;; Editing
-        ;;multiple-cursors expand-region undo-tree browse-kill-ring
-        ;;fill-column-indicator
-
-        ;; Navigation
-        ;;ace-jump-mode jump-char minimap
-        anything
-
-        ;; Themes
-        color-theme-solarized
-
-        ;; dirtree
-        ;;dirtree
-        tree-mode
-        windata
-        popwin
-	))
-(el-get 'sync el-get-sources)
+;; Key Bindings
+(setq ns-command-modifier (quote meta))
+(setq ns-alternate-modifier (quote super))
 
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-w" 'backward-kill-word)
 (global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-h" 'delete-backward-char)
+(global-set-key "\C-cg" 'magit-status)
+(global-set-key "\M-g" 'goto-line)
+(global-set-key "\C-w" 'kill-region)
 
-;;(push "~/.emacs.d" load-path)
-(push "~/.emacs.d/lisp" load-path)
-(push "~/.emacs.d/bin" exec-path)
+(setq-default tab-width 2 indent-tabs-mode nil)
+
+;; ウインドウを表示する
+;; http://openlab.dino.co.jp/2007/11/20/110609148.html
+(global-set-key "\M-2" 'make-frame)
+(global-set-key "\M-0" 'delete-frame)
+
+;; beepを無視する
+(setq ring-bell-function 'ignore)
+
+;; for Mac
+(if (eq system-type 'darwin)
+    (setq mac-command-modifier 'meta))
+
+(require 'server)
+(or (server-running-p)
+    (server-start))
+(set-language-environment 'Japanese)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(prefer-coding-system 'utf-8-unix)
+
+;; Do not show the splash screen or message
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-echo-area-message t)
+
+;; Show row and column numbers
+(setq line-number-mode t)
+(setq column-number-mode t)
+
+;; Display the current time and system load
+(load-library "time")
+(setq display-time-24hr-format t
+      display-time-form-list (list 'time 'load)
+      display-time-day-and-date t)
+(display-time)
+(blink-cursor-mode t)
+
+;;行番号表示
+(require 'linum)
+(global-linum-mode)
+
+;;画面サイズ
+(setq initial-frame-alist
+      '((top . 1) (left . 1) (width . 160) (height . 50)))
+
+;; from http://sakito.jp/emacs/emacs23.html
+(when (display-graphic-p)
+  ;; color-theme
+  (require 'color-theme)
+  (color-theme-initialize)
+  (color-theme-midnight)
+  ;; Font
+  (create-fontset-from-ascii-font "Menlo-12:weight=normal:slant=normal" nil "menlokakugo")
+  (set-fontset-font "fontset-menlokakugo"
+                    'unicode
+                    (font-spec :family "Hiragino Kaku Gothic ProN" :size 14)
+                    nil
+                    'append)
+  (add-to-list 'default-frame-alist '(font . "fontset-menlokakugo"))
+)
+;; Override Darwin
+(set-face-foreground 'font-lock-comment-face "red")
+(set-face-foreground 'font-lock-comment-delimiter-face "red")
 
 
+;; Remove all the mouse-assisted crud
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+;;(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
-;; popwinの設定
-(require 'popwin)
-(setq display-buffer-function 'popwin:display-buffer)
-(setq popwin:popup-window-position 'bottom)
+
+;; Highlighting "TODO", "FIXME" and friends
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (font-lock-add-keywords nil
+                                    '(("\\<\\(FIXME\\|TODO\\|BUG\\):"
+                                       1 font-lock-warning-face t)))))
 
 
-(load "emacs.el")
-(load "modes.el")
-(load "utilities.el")
-(load "bindings.el")
-(load "themes.el")
+;; Show matching parenthesis
+(show-paren-mode t)
+
+
+;; Set random parameters
+(setq search-highlight t        ; Highlight all visible matches
+      query-replace-highlight t ; Highlight all visible matches
+      transient-mark-mode t)    ; Perform certain commands only on the marked region
+
+
+;; Properly display colors in shell
+(ansi-color-for-comint-mode-on)
+
+
+;; Set a better font if running in windows
+(if (eq system-type 'windows-nt)
+    (set-default-font "consolas-10"))
+
+
+;; helm-mode
+(require 'helm-config)
+(require 'helm-ag)
+(require 'helm-descbinds)
+
+(helm-descbinds-mode)
+
+(global-set-key (kbd "C-;") 'helm-mini)
+(global-set-key (kbd "C-c h") 'helm-mini)
+(global-set-key (kbd "C-c b") 'helm-descbinds)
+(global-set-key (kbd "C-c o") 'helm-occur)
+(global-set-key (kbd "C-c s") 'helm-ag)
+(global-set-key (kbd "C-c y") 'helm-show-kill-ring)
+
+;;flycheck
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; magit-mode
+(require 'magit)
+(global-set-key (kbd "C-c v") 'magit-status)
+
+;; wakatime-mode
+(global-wakatime-mode)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 ;; If ~/.emacs.local is available load it as the last file so that it is
 ;; possible to add local settings and overrides.
-(if (file-readable-p (expand-file-name "~/.emacs.local"))
-    (load-file (expand-file-name "~/.emacs.local"))
+(if (file-readable-p (expand-file-name "~/.local.el"))
+    (load-file (expand-file-name "~/.local.el"))
   nil)
 
-
-;; trampの設定: /sudo:
-(add-to-list 'tramp-default-proxies-alist
-             '(nil "\\`root\\'" "/ssh:%h:"))
-(add-to-list 'tramp-default-proxies-alist
-             '("localhost" nil nil))
-(add-to-list 'tramp-default-proxies-alist
-             '((regexp-quote (system-name)) nil nil))
+;;
+;; Ruby mode
+;;
+;; smart-newline
+(add-hook 'ruby-mode-hook ;; or any major-mode-hooks
+  (lambda ()
+  (smart-newline-mode t)))
