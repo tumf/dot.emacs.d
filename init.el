8520fda552207932cf21ea1016c38010b53cc138
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2015-08-24 05:21:45 +0900

    文字コードの設定

--- init.el
+++ init.el
@@ -25,6 +25,33 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
+;;
+;; 文字コードの設定
+;;
+(setenv "LANG" "ja_JP.UTF-8")
+(set-language-environment 'utf-8)
+(prefer-coding-system 'utf-8)
+
+(cond
+ ((or (eq window-system 'mac) (eq window-system 'ns))
+  ;; Mac OS X の HFS+ ファイルフォーマットではファイル名は NFD (の様な物)で扱うため以下の設定をする必要がある
+  (require 'ucs-normalize)
+  (setq file-name-coding-system 'utf-8-hfs)
+  (setq locale-coding-system 'utf-8-hfs))
+ (or (eq system-type 'cygwin) (eq system-type 'windows-nt)
+     (setq file-name-coding-system 'utf-8)
+     (setq locale-coding-system 'utf-8)
+     ;; もしコマンドプロンプトを利用するなら sjis にする
+     ;; (setq file-name-coding-system 'sjis)
+     ;; (setq locale-coding-system 'sjis)
+     ;; 古い Cygwin だと EUC-JP にする
+     ;; (setq file-name-coding-system 'euc-jp)
+     ;; (setq locale-coding-system 'euc-jp)
+     )
+ (t
+  (setq file-name-coding-system 'utf-8)
+  (setq locale-coding-system 'utf-8)))
+
 ;; 外部コマンドのPATH
 ;; この設定が利用する主なコマンド `git` `ag`
 (push "/usr/local/bin" exec-path)
