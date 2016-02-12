b508b196552297a81e1eca2c2a59f5eeaca302de
tumf <y.takahara@gmail.com>
2016-02-12 10:28:07 +0900

    Add ruby-beautify-buffer C-x C-s

--- config.el
+++ config.el
@@ -293,6 +293,23 @@
   (smart-newline-mode t)))
 (rvm-use-default)
 
+(defun ruby-beautify-buffer ()
+  (interactive)
+  (let (p rb)
+      (setq p (point) rb (buffer-string))
+
+      (with-temp-buffer
+        (insert rb)
+        (call-process-region (point-min) (point-max) "rbeautify" t t)
+        (setq rb (buffer-string)))
+
+      (erase-buffer)
+      (insert rb)
+      (goto-char p)))
+
+(eval-after-load 'ruby-mode
+  '(define-key ruby-mode-map (kbd "C-x C-s") 'ruby-beautify-buffer))
+
 ;;
 ;; Multi-term
 ;;
