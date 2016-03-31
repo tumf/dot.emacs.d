8e337079955a87bd607517ce783ba6106e659c09
tumf <y.takahara@gmail.com>
2016-03-31 08:29:14 +0700

    Comment out Japanese.
    refs #{issue}

--- config.el
+++ config.el
@@ -90,25 +90,25 @@
 (set-language-environment 'utf-8)
 (prefer-coding-system 'utf-8)
 
-(cond
- ((or (eq window-system 'mac) (eq window-system 'ns))
-  ;; Mac OS X の HFS+ ファイルフォーマットではファイル名は NFD (の様な物)で扱うため以下の設定をする必要がある
-  (require 'ucs-normalize)
-  (setq file-name-coding-system 'utf-8-hfs)
-  (setq locale-coding-system 'utf-8-hfs))
- (or (eq system-type 'cygwin) (eq system-type 'windows-nt)
-     (setq file-name-coding-system 'utf-8)
-     (setq locale-coding-system 'utf-8)
-     ;; もしコマンドプロンプトを利用するなら sjis にする
-     ;; (setq file-name-coding-system 'sjis)
-     ;; (setq locale-coding-system 'sjis)
-     ;; 古い Cygwin だと EUC-JP にする
-     ;; (setq file-name-coding-system 'euc-jp)
-     ;; (setq locale-coding-system 'euc-jp)
-     )
- (t
-  (setq file-name-coding-system 'utf-8)
-  (setq locale-coding-system 'utf-8)))
+;;(cond
+;; ((or (eq window-system 'mac) (eq window-system 'ns))
+;;  ;; Mac OS X の HFS+ ファイルフォーマットではファイル名は NFD (の様な物)で扱うため以下の設定をする必要がある
+;;  (require 'ucs-normalize)
+;;  (setq file-name-coding-system 'utf-8-hfs)
+;;  (setq locale-coding-system 'utf-8-hfs))
+;; (or (eq system-type 'cygwin) (eq system-type 'windows-nt)
+;;     (setq file-name-coding-system 'utf-8)
+;;     (setq locale-coding-system 'utf-8)
+;;     ;; もしコマンドプロンプトを利用するなら sjis にする
+;;     ;; (setq file-name-coding-system 'sjis)
+;;     ;; (setq locale-coding-system 'sjis)
+;;     ;; 古い Cygwin だと EUC-JP にする
+;;     ;; (setq file-name-coding-system 'euc-jp)
+;;     ;; (setq locale-coding-system 'euc-jp)
+;;     )
+;; (t
+;;  (setq file-name-coding-system 'utf-8)
+;;  (setq locale-coding-system 'utf-8)))
 
 ;; 外部コマンドのPATH
 ;; この設定が利用する主なコマンド `git` `ag`
